1cbff7006d3f6e91a33a9ff6877cba60c8acf514
Naphtal USABYUWERA <usanaphtal112@gmail.com>
2025-07-29 03:33:15 +0530

    Fix Vercel deployment

--- build_files.sh
+++ build_files.sh
@@ -3,14 +3,14 @@
 echo "--------------START BUILD-----------"
 
 echo "Building Project Packages........"
-python3.9 -m pip install --upgrade pip
-python3.9 -m pip install -r requirements.txt
+python3.10 -m pip install --upgrade pip
+python3.10 -m pip install -r requirements.txt
 
 echo "Collect static files"
-python3.9 manage.py collectstatic --noinput
+python3.10 manage.py collectstatic --noinput
 
 echo "Migrating the Databases........."
 # python3 manage.py makemigrations --noinput
-python3.9 manage.py migrate --noinput
+python3.10 manage.py migrate --noinput
 
 echo "--------------END OF BUILD-----------"
