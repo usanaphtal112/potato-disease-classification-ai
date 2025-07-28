a548448074e2ca9966603cee85aee8fd4b32eb61
Naphtal USABYUWERA <usanaphtal112@gmail.com>
2025-07-29 00:23:27 +0530

    Fix Vercel deployment

--- potatoes_classifier_project/settings.py
+++ potatoes_classifier_project/settings.py
@@ -140,6 +140,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles", "static")
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 # Model configuration
 MODEL_PATH = os.path.join(BASE_DIR, "models", "potato_disease_float16.onnx")
 TARGET_IMAGE_SIZE = (224, 224)
@@ -179,14 +181,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "static/"
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 SITE_ID = 1
 
 # Cloudinary settings
