37686a89989f7b6131213e94e91dcd4a31468a56
Naphtal USABYUWERA <usanaphtal112@gmail.com>
2025-07-29 01:04:37 +0530

    Fix Vercel deployment

--- build_files.sh
+++ build_files.sh
@@ -2,14 +2,6 @@
 
 echo "--------------START BUILD-----------"
 
-# Check if pip3 is available
-if command -v pip3 &> /dev/null; then
-    echo "pip3 is installed. Version:"
-    pip3 --version
-else
-    echo "pip3 is not installed"
-fi
-
 echo "Building Project Packages........"
 python3.9 -m pip install --upgrade pip
 python3.9 -m pip install -r requirements.txt
